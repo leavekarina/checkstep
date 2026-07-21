1fd0ede5c7db6e79b41c5e220046420911683708
Karina <whimsy.leaven-6t@icloud.com>
2026-07-21 22:33:15 +0200

    Stage 29: Добавь конфигурацию приложения через словарь настроек

---
+++ clean_routine-29.py
@@ -0,0 +1,10 @@
+# === Stage 29: Добавь конфигурацию приложения через словарь настроек ===
+# Project: CleanRoutine
+APP_CONFIG = {
+    "app_name": "CleanRoutine",
+    "version": 1,
+    "default_zone_interval_days": 7,
+    "stats_retention_days": 30,
+    "notification_enabled": True,
+    "log_level": "info",
+}
